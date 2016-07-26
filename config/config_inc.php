99793914feaa58244ad1b19aff438b7d39646876
urchin <hikaru.miro@hotmail.com>
2016-07-26 23:28:21 +0200

    changed the font colour

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantis';
+$g_db_username            = 'root';
+$g_db_password            = 'root';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'QfzAAImSphr6oMG+f/rcn/ZT/RUX+2F6l7XmOq6heoM=';
